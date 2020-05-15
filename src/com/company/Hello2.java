06697bf21c76574dfb93b44f33093a1036d87b81
shuuhei777 <shuuhei777@gmail.com>
2020-05-16 06:38:44 +0900

    2回目

---
+++ src/com/company/Hello2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Hello2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hello World!");
+    }
+
+}
