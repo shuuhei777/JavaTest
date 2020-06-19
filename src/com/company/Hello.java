8999e7b4d5a9698bc8e87f019339c4c432cb53b9
shuuhei777 <shuuhei777@gmail.com>
2020-06-19 21:14:09 +0900

    2回目

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -21,8 +21,8 @@ public class Hello {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for(int i=0;i<10;i++)
-        System.out.println("Hello World!");
+        for(int i=0;i<100;i++)
+            System.out.println("Hello World!");
     }
 
 }
